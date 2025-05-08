cc57012b2ce7ff2cd27f086ad6cdfb74602c26fa
zinhle <ngwanejeanette3@gmail.com>
2025-05-08 22:21:29 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
 
 import './App.css';
 import quizData from './quizData';
-// import { useState } from 'react';
+import { useState } from 'react';
 
 
 function App() {
 
 return(
+  <>
     <div className="App">
- <h2>{quizData.question}</h2>
- <ul>
-        {quizData.options.map((option, index) => (
-          <li key={index}>{option}</li> ))}
-          </ul>
+ <h2>{quizData[0].question}</h2>
+ 
     </div>
+    <div>
+      <h2>{quizData[1].question}</h2>
+    </div>
+    <div>  
+        <h2>{quizData[2].question}</h2>
+    </div>
+     <div>  
+        <h2>{quizData[3].question}</h2>
+    </div>
+     <div>  
+        <h2>{quizData[4].question}</h2>
+    </div>
+    </>
   );
 }
 
--- src/quizData.js
+++ src/quizData.js
@@ -3,17 +3,33 @@ import React from 'react'
 
 
 
- const quizData = 
+const quizData = [
   {
-   question: "What is the capital of France?",
-   options: ["Paris","Madrid","Rome","Berlin"],
-   answer: "Paris",
-  };
- //  {
- //   question: "What is my current province?",
- //   options: ["Gauteng","Mpumalanga","Eastern Cape","KZN"],
- //   answer: "Gauteng",
- //  }
- // ];
+    question: "What is the capital of France?",
+    options: ["Paris", "Madrid", "Rome", "Berlin"],
+    answer: "Paris",
+  },
+  {
+    question: "What is my current province?",
+    options: ["Gauteng", "Mpumalanga", "Eastern Cape", "KZN"],
+    answer: "Gauteng",
+  },
+   {
+    question: "What is my current province?",
+    options: ["Gauteng", "Mpumalanga", "Eastern Cape", "KZN"],
+    answer: "Gauteng",
+  },
+   {
+    question: "What is my current province?",
+    options: ["Gauteng", "Mpumalanga", "Eastern Cape", "KZN"],
+    answer: "Gauteng",
+  },
+   {
+    question: "What is my current province?",
+    options: ["Gauteng", "Mpumalanga", "Eastern Cape", "KZN"],
+    answer: "Gauteng",
+  }
+];
+
 
 export default quizData; 
