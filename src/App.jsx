ccc3415b3ed3cea7f6e68024b63adb396b1fd4e2
zinhle <ngwanejeanette3@gmail.com>
2025-05-11 13:01:12 +0200

    Added questions and result part

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,56 @@ import { useState } from 'react';
 
 
 function App() {
+const [options, setOptions] = useState("");
+const [Correct, setCorrect] = useState("null");
+const [ Questions, setQuestions] = useState(0);
+const handleclickOption = (option) => {
+  setOptions(option);
+  setCorrect(option === quizData[Questions].answer
+  );
+}
 
 return(
   <>
-    <div className="App">
+  <h1 className="heading">Welcome to Quiz-Game</h1>
+    <div className="questions">
  <h2>{quizData[0].question}</h2>
- 
+
+  {quizData[0].options.map((option, index) => (
+    (<li>
+     <button onClick={() => handleclickOption(option)}>{option}</button>
+    </li>)))
+  }
+  {setOptions[Questions] && (
+<p>
+{Correct ? "Good job!" : "Sorry"}</p>
+  )}
+<h2>{quizData[1].question}</h2>
+
+  {quizData[1].options.map((option, index) => (
+    (<li>
+     <button className="btn" onClick={() => handleclickOption(option)}>{option}</button>
+    </li>)))
+  }
+  {setOptions[Questions] && (
+<p>
+{Correct ? "Good job!" : "Sorry"}</p>
+  )}
+
     </div>
-    <div>
+ 
+    {/* <div className="questions">
       <h2>{quizData[1].question}</h2>
     </div>
-    <div>  
-        <h2>{quizData[2].question}</h2>
+    <div className="questions">  
+        <h2 >{quizData[2].question}</h2>
     </div>
-     <div>  
+     <div className="questions">  
         <h2>{quizData[3].question}</h2>
     </div>
-     <div>  
+     <div className="questions">  
         <h2>{quizData[4].question}</h2>
-    </div>
+    // </div> */}
     </>
   );
 }
